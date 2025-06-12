dec19a02a02d4ce9cd7afc0a243ee63011da7c12
Nicolas Megow <nicolas.megow@strategiepark.de>
2025-06-12 09:37:18 +0200

    Version 1.0

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       includes: "includes",
-      output: "_site",
+      output: "docs",
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
